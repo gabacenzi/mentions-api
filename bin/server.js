946025e07d550076d541d7d25a53e38c9c7ea12c
Gabriel Amelio Vicenzi <gabacenzi@gmail.com>
2020-01-30 17:21:58 -0300

    nova pasta e alteração arquivo

--- bin/server.js
+++ bin/server.js
@@ -9,14 +9,45 @@ function normalalizePort(val) {
   if (isNaN(port)) {
     return val;
   }
-
   if (port >= 0) {
     return port;
   }
-
   return false;
-
 }
 
 const port = normalalizePort(process.env.PORT || 3000);
 app.set('port',port);
+
+//error handler
+function onError(error) {
+  if (erro.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACESS':
+      console.error(bind + ' requeries elevated privileges');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is alread in use');
+      process.exit(1);
+    default:
+      throw error;
+  }
+}
+
+//listener handler
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'Pipe ' + addr : 'Port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+//server
+const server = http.createServer(app);
+server.listen(port);
+server.on('error',onError);
+server.on('listening',onListening);
+console.log(`API is alive on ${port}`);
+
