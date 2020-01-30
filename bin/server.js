828a9689919013cf87a768f18e35fc678fa8a5b0
Gabriel Amelio Vicenzi <gabacenzi@gmail.com>
2020-01-30 13:55:23 -0300

    novos arquivos e alteração server.js

--- bin/server.js
+++ bin/server.js
@@ -1,1 +1,22 @@
-teste envio arquivo alterado branch desenv
+const app = require('../src/app');
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+
+//PORT //based on express-generator
+function normalalizePort(val) {
+  const port = parseInt(val,10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+
+}
+
+const port = normalalizePort(process.env.PORT || 3000);
+app.set('port',port);
